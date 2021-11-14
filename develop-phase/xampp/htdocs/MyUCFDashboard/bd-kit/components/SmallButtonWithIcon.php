af75b397aa0ec9578f42ccb4c29ac73226b283d1
mikudayo <benjamin@benjaminduckworth.com>
2021-11-14 00:28:26 -0500

    navbar buttons

---
+++ develop-phase/xampp/htdocs/MyUCFDashboard/bd-kit/components/SmallButtonWithIcon.php
@@ -0,0 +1,31 @@
+<?php
+
+$path = ["bd-kit","Component.php"];
+include_once(join(DIRECTORY_SEPARATOR, $path));
+
+class SmallButtonWithIcon extends BDComponent {
+
+    function __construct() {
+
+        $arg_children = func_get_args();
+        $title_string = array_shift($arg_children);
+        $href_url = array_shift($arg_children);
+        $fa_icon = array_shift($arg_children);
+        $this->children = ["<div style=\"display:inline-block;\">",
+                                "<a href=\"".$href_url."\" class=\"smallbuttonwithicon\">",
+                                    "<i class=\"fa ".$fa_icon."\"></i>",
+                                    " ",
+                                    $title_string,
+                                "</a>",
+                           "</div>"];
+        if (count($arg_children) == 0) {
+            return;
+        } else {
+            $this->children = [$this->children[0], ...$arg_children, $this->children[1]];
+            return;
+        }
+    }
+
+}
+
+?>
--- develop-phase/xampp/htdocs/MyUCFDashboard/index.php
+++ develop-phase/xampp/htdocs/MyUCFDashboard/index.php
@@ -50,10 +50,24 @@ $path = ["bd-kit","components","ExpandedCard.php"];
 include_once(join(DIRECTORY_SEPARATOR, $path));
 $path = ["bd-kit","components","VSpacer.php"];
 include_once(join(DIRECTORY_SEPARATOR, $path));
+$path = ["bd-kit","components","SmallButtonWithIcon.php"];
+include_once(join(DIRECTORY_SEPARATOR, $path));
 
 $page_elements = [
     "<div>",
-    new NavSidebar(),
+    new NavSidebar(
+        "<div style=\"top:0;position:absolute;\">",
+            new VSpacer("20px"),
+            new SmallButtonWithIcon("Profile", "./profile.php", "fa-user-circle"),
+            new VSpacer("20px"),
+            new SmallButtonWithIcon("Home", "./index.php", "fa-home"),
+            new VSpacer("20px"),
+        "</div>",
+        "<div style=\"bottom:0;position:absolute;\">",
+            new SmallButtonWithIcon("Settings", "./settings.php", "fa-cog"),
+            new VSpacer("20px"),
+        "</div>",
+    ),
     new MainContent(
         new FlexRow(
             new VSpacer("20px")
