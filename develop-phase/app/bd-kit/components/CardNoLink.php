6bbc52ad2bcacd55798428fe40bb273fb1ccf2a9
mikudayo <benjamin@benjaminduckworth.com>
2021-11-18 00:22:45 -0500

    fixed card resize issue via css

--- develop-phase/app/bd-kit/components/CardNoLink.php
+++ develop-phase/app/bd-kit/components/CardNoLink.php
@@ -17,20 +17,20 @@ class CardNoLink extends BDComponent {
         $arg_children = func_get_args();
         $title_string = array_shift($arg_children);
         if (count($arg_children) == 0) {
-            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;height:70px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
+            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;min-height:70px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
                                 "<div style=\"width:auto;\">",
                                     "<h6 style=\"padding:5px;line-height:1;\"><b>".$title_string."</b></h6>",
                                 "</div>",
-                                "<div style=\"height:24px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
+                                "<div style=\"min-height:24px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
                                 "</div>",
                            "</div>"];
             return;
         } else {
-            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;height:70px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
+            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;min-height:70px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
                                 "<div style=\"width:auto;\">",
                                     "<h6 style=\"padding:5px;line-height:1;\"><b>".$title_string."</b></h6>",
                                 "</div>",
-                                "<div style=\"height:24px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
+                                "<div style=\"min-height:24px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
                                     ...$arg_children,
                                 "</div>",
                            "</div>"];
--- develop-phase/app/bd-kit/components/ExpandedCard.php
+++ develop-phase/app/bd-kit/components/ExpandedCard.php
@@ -19,11 +19,11 @@ class ExpandedCard extends BDComponent {
         $url_string = array_shift($arg_children);
         $url_href = array_shift($arg_children);
         if (count($arg_children) == 0) {
-            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;height:270px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
+            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;min-height:270px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
                                 "<div style=\"width:auto;\">",
                                     "<h6 style=\"padding:5px;line-height:1;\"><b>".$title_string."</b></h6>",
                                 "</div>",
-                                "<div style=\"height:200px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
+                                "<div style=\"min-height:200px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
                                 "</div>",
                                 "<div style=\"width:auto;\">",
                                     "<a href=\"".$url_href."\" style=\"font-weight:500;color:".$LINK_COLOR.";text-align:right;\">",
@@ -35,11 +35,11 @@ class ExpandedCard extends BDComponent {
                            "</div>"];
             return;
         } else {
-            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;height:270px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
+            $this->children = ["<div style=\"display:block;flex-grow:1;width:400px;min-height:270px;color:".$TEXT_COLOR.";background-color:".$CARD_BG_COLOR.";border-radius:10px;margin:10px;\">",
                                 "<div style=\"width:auto;\">",
                                     "<h6 style=\"padding:5px;line-height:1;\"><b>".$title_string."</b></h6>",
                                 "</div>",
-                                "<div style=\"height:200px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
+                                "<div style=\"min-height:200px;width:auto;background-color:".$TRANSPARENT_BG_COLOR.";\">",
                                     ...$arg_children,
                                 "</div>",
                                 "<div style=\"width:auto;\">",
