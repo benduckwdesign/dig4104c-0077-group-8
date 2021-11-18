00b48bce54888df3badc698017c0cb794e937719
mikudayo <benjamin@benjaminduckworth.com>
2021-11-17 22:41:33 -0500

    side navbar overhaul
    added more links to navbar and to urls table based on page tables

---
+++ develop-phase/app/bd-kit/components/FrameFlexRow.php
@@ -0,0 +1,27 @@
+<?php
+
+$path = [__DIR__,"..","Component.php"];
+include_once(join(DIRECTORY_SEPARATOR, $path));
+$path = [__DIR__,"..","..","backend","ThemeColors.php"];
+include_once(join(DIRECTORY_SEPARATOR, $path));
+
+class FrameFlexRow extends BDComponent {
+
+    function __construct() {
+        GLOBAL $TEXT_COLOR;
+        GLOBAL $BACKGROUND_COLOR;
+        GLOBAL $TRANSPARENT_BG_COLOR;
+
+        $arg_children = func_get_args();
+        $this->children = ["<div style=\"display:flex;flex-direction:row;color:".$TEXT_COLOR.";flex-wrap:nowrap;height:calc(100vh - 100px);\">", "</div>"];
+        if (count($arg_children) == 0) {
+            return;
+        } else {
+            $this->children = [$this->children[0], ...$arg_children, $this->children[1]];
+            return;
+        }
+    }
+
+}
+
+?>
